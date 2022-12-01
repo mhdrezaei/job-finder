48256e156ef411ccedb534109c268e4cfc4ca41b
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-12-01 17:35:50 +0330

    some style

--- src/componnents/ui/Hero.js
+++ src/componnents/ui/Hero.js
@@ -1,10 +1,12 @@
 import React from "react";
-import './Hero.css'
+import "./Hero.css";
 function Hero({ children }) {
   return (
     <div className="hero">
       <div className="col-md-12">
-        <div className="row justify-content-center">{children}</div>
+        <div className="container">
+          <div className="row justify-content-center">{children}</div>
+        </div>
       </div>
     </div>
   );
