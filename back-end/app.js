6089829495af3f13513a6e0caef4f452c6a21e4c
رضائی - محمد <rezaee-moha@tehran.iri>
2022-12-03 01:57:15 +0330

    add middleware

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,34 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
 const dotenv = require("dotenv");
+const cors = require('cors');
 const databaseConnection = require("./config/databaseConnection");
+const jobs = require("./routes/jobs");
+const errorMiddleware = require("./middlewares/errors");
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+ app.use(bodyParser.json());
+
+
 
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT;
 
-
+databaseConnection();
 // add json body parser
 app.use(express.json());
+
+// all routes
+app.use("/api/v1/", jobs);
+
+app.use(errorMiddleware);
 
 
 
